7a2fe367d0a343e2855510b89e676f47e6d8cb2f
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2022-11-08 07:49:39 +0200

    Added file listing result structure

--- src/main/java/com/aubrey/filereader/models/FileListResult.java
+++ src/main/java/com/aubrey/filereader/models/FileListResult.java
@@ -1,4 +1,19 @@
 package com.aubrey.filereader.models;
 
+import lombok.Getter;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
 public class FileListResult {
+    private String FilePath;
+    private LocalDateTime CreationTime;
+    private LocalDateTime LastAccessTime;
+    private LocalDateTime LastModifiedTime;
+    private Long Size;
+    private boolean IsRegularFile;
+    private boolean IsDirectory;
+    private boolean IsOther;
+    private boolean IsSymbolicLink;
 }
